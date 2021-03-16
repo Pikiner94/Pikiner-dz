d1ea2c356975db7c035542405f17e6700b5d063d
Eugene <evgeniykip94@gmail.com>
2021-03-16 16:44:29 +0200

    test2

--- task2.js
+++ task2.js
@@ -8,7 +8,7 @@ let total = 80
 let ordered = 20
 
 if (total>ordered){console.log ("Заказ оформлен, с вами свяжется менеджер.")}
-else {console.log ("На складе недостаточно товаров!")}
+else {console.log ("На складе недостаточно товаров!") }
 
 
 // let total = 80
